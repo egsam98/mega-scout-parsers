a1772f620f9d5a95e136475727877b0885db8784
egsam98 <egsam98@gmail.com>
2020-07-10 23:35:35 +0300

    Парсинг лиг

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -13,6 +14,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/countries", parseCountries)
 	r.GET("/seasons", parseSeasons)
+	r.GET("/leagues", parseLeagues)
 	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
 		log.Fatal(err)
 	}
@@ -31,4 +33,29 @@ func parseSeasons(c *gin.Context) {
 	seasons := parsers.Seasons()
 	c.Header("Content-Type", "application/json")
 	c.JSON(200, seasons)
+}
+
+func parseLeagues(c *gin.Context) {
+	countryId, err := strconv.Atoi(c.Query("country"))
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid country. Must be integer.",
+		})
+		return
+	}
+	seasonPeriod, err := strconv.Atoi(c.Query("season_period"))
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid season_period. Must be integer.",
+		})
+		return
+	}
+	leagues, err := parsers.Leagues(countryId, seasonPeriod)
+	if err != nil {
+		c.JSON(408, gin.H{
+			"error": err,
+		})
+		return
+	}
+	c.JSON(200, leagues)
 }
---
+++ parsers/leagues.go
@@ -0,0 +1,78 @@
+package parsers
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const BaseUrl = "https://transfermarkt.com"
+
+func Leagues(countryId, seasonPeriod int) ([]gin.H, error) {
+	url := fmt.Sprintf("%s/wettbewerbe/national/wettbewerbe/%d?saison_id=%d", BaseUrl, countryId, seasonPeriod)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %s", url, err)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]gin.H, 0)
+	tier := ""
+	doc.Find("#yw1 tbody > tr").Each(func(i int, tr *goquery.Selection) {
+		if strings.Contains(tr.Text(), "Cup") {
+			return
+		}
+		td := tr.Find("td").First()
+		tdClass, _ := td.Attr("class")
+		if strings.Contains(tdClass, "extrarow") {
+			tier = td.Text()
+			return
+		}
+		if _, exists := tr.Attr("class"); !exists {
+			return
+		}
+		a := td.Find("a").Last()
+		href, _ := a.Attr("href")
+		logoStr, exists := td.Find("img").First().Attr("src")
+		logo := interface{}(logoStr)
+		if exists {
+			logo = strings.ReplaceAll(logoStr, "tiny", "normal")
+		} else {
+			logo = nil
+		}
+		regex, _ := regexp.Compile(`/saison_id/\d+/?`)
+		urlWithoutSeasonId := regex.Split(href, 2)[0]
+		splitted := strings.Split(urlWithoutSeasonId, "/")
+		result = append(result, gin.H{
+			"id":       generateId(splitted[len(splitted)-1]),
+			"url":      BaseUrl + urlWithoutSeasonId,
+			"title":    strings.Trim(td.Text(), "\t\n "),
+			"logo":     logo,
+			"position": tier,
+		})
+	})
+	return result, nil
+}
+
+func generateId(id string) int {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	result := ""
+	for _, str := range id {
+		str := string(str)
+		_, err := strconv.Atoi(str)
+		if err != nil {
+			result += strconv.Itoa(strings.Index(alphabet, str))
+		} else {
+			result += str
+		}
+	}
+	resultNum, _ := strconv.Atoi(result)
+	return resultNum
+}
